007bc91f628b3900d8a3f94feecc1f603b0411c3
Japanmaluco <eduardobessadesouza@gmail.com>
2022-03-16 16:13:42 -0300

    first

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const app = http.createServer(requestHandler);
 app.listen(APP_PORT);
 console.log(`🖥 HTTP Server running at ${APP_PORT}`);
 
-// handles all http requests to the server
 function requestHandler(request, response) {
   console.log(`🖥 Received request for ${request.url}`);
-  // append /client to serve pages from that folder
+
   var filePath = "./client" + request.url;
   if (filePath == "./client/") {
-    // serve index page on request /
     filePath = "./client/index.html";
   }
   var extname = String(path.extname(filePath)).toLowerCase();
@@ -46,14 +44,11 @@ function requestHandler(request, response) {
   });
 }
 
-// SOCKET.IO CHAT EVENT HANDLING
 const io = require("socket.io")(app, {
   path: "/socket.io",
 });
 
 io.attach(app, {
-  // includes local domain to avoid CORS error locally
-  // configure it accordingly for production
   cors: {
     origin: "http://localhost",
     methods: ["GET", "POST"],
@@ -72,14 +67,12 @@ var users = {};
 io.on("connection", (socket) => {
   console.log("👾 New socket connected! >>", socket.id);
 
-  // handles new connection
   socket.on("new-connection", (data) => {
-    // captures event when new clients join
     console.log(`new-connection event received`, data);
-    // adds user to list
+
     users[socket.id] = data.username;
     console.log("users :>> ", users);
-    // emit welcome message event
+
     socket.emit("welcome-message", {
       user: "server",
       message: `Welcome to this Socket.io chat ${data.username}. There are ${
@@ -88,10 +81,9 @@ io.on("connection", (socket) => {
     });
   });
 
-  // handles message posted by client
   socket.on("new-message", (data) => {
     console.log(`👾 new-message from ${data.user}`);
-    // broadcast message to all sockets except the one that triggered the event
+
     socket.broadcast.emit("broadcast-message", {
       user: users[data.user],
       message: data.message,
